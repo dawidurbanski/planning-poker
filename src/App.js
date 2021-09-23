32cbe2efe1ec5cdc7ea411db21d4073d09c87648
dawidurbanski <hi@dawidurbanski.com>
2021-09-23 23:16:40 +0200

    React app boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Planning Poker
+      </header>
+    </div>
+  );
+}
+
+export default App;
